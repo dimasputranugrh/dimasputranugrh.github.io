9c3f65f00045fb7fe1e8fe63ca05a87446e6a855
dimas putra nugraha <dimasputranugrh@gmail.com>
2021-01-09 19:05:34 +0700

    v2

--- js/main.js
+++ js/main.js
@@ -1,169 +1,413 @@
-/*global $, jQuery, alert*/
-$(document).ready(function () {
+/*------------------------------------
+ *Author:FortunaTheme
+ *Template:Rivo
+ *Version:1.1
+ *-------------------------------------
+ */
+(function($) {
 
-  'use strict';
+    "use strict";
 
-  // ========================================================================= //
-  //  //SMOOTH SCROLL
-  // ========================================================================= //
-
-
-  $(document).on("scroll", onScroll);
-
-  $('a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('a').each(function () {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
-
-    $(this).addClass('active');
-
-    var target = this.hash,
-      menu = target;
-
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function () {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
+    jQuery(document).on("ready", function() {
 
 
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
 
-  // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
+        /*
+         * -----------------------------------------------------------------
+         *---------------------------Preloader and Anchor Tag---------------
+         * -----------------------------------------------------------------
+         */
 
+        var themeWindow = $(window);
+        var pagebody = $('html, body');
+        themeWindow.on("load", function() {
 
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
+            var preloader = jQuery('.preloader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(200).fadeOut('slow');
+            themeWindow.scrollTop(0);
+        });
 
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
-
-  $('.responsive').on('click', function (e) {
-    $('.nav-menu').slideToggle();
-  });
-
-  // ========================================================================= //
-  //  Typed Js
-  // ========================================================================= //
-
-  var typed = $(".typed");
-
-  $(function () {
-    typed.typed({
-      strings: [" SOFTWARE DEVELOPMENT.", "WEB DESIGNER.", "UI/UX DEVELOPER."],
-      typeSpeed: 100,
-      loop: true,
-    });
-  });
+        var anchor = $('a[href="#"]');
+        anchor.on("click", function() {
+            e.preventDefault();
+        });
 
 
-  // ========================================================================= //
-  //  Owl Carousel Services
-  // ========================================================================= //
+        /*
+         * -----------------------------------------------------------------
+         *-------------------single-page-nav-plugin------------------------
+         * -----------------------------------------------------------------
+         */
 
 
-  $('.services-carousel').owlCarousel({
-    autoplay: false,
-    loop: true,
-    margin: 20,
-    dots: true,
-    nav: false,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
 
-  // ========================================================================= //
-  //  magnificPopup
-  // ========================================================================= //
+        // Prevent console.log from generating errors in IE for the purposes of the demo
+        if(!window.console) console = {
+            log: function() {}
+        };
 
-  var magnifPopup = function () {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
+        // The actual plugin
+        if($(".welcome-area").is("#welcome-area")) {
+            var singleNav = jQuery('.single-page-nav');
+            singleNav.singlePageNav({
+                offset: singleNav.outerHeight(),
+                filter: ':not(.external)',
+                updateHash: false
+            });
 
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+            /*
+             * -----------------------------------------------------------------
+             *-------------------------Skill------------------------------------
+             * -----------------------------------------------------------------
+             */
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function (openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+            var skill = $('.about');
+
+            var width1 = $(".prog1").data("progress");
+            var progBar1 = $(".prog1");
+            skill.waypoint(function() {
+                progBar1.css({
+                    "width": width1,
+                    "transition": "2s ease-in"
+                });
+            }, {
+
+                offset: width1
+            });
+
+
+            var width2 = $(".prog2").data("progress");
+            var progBar2 = $(".prog2");
+            skill.waypoint(function() {
+                progBar2.css({
+                    "width": width2,
+                    "transition": "2s ease-in"
+                });
+            }, {
+
+                offset: width2
+            });
+
+
+            var width3 = $(".prog3").data("progress");
+            var progBar3 = $(".prog3");
+            skill.waypoint(function() {
+                progBar3.css({
+                    "width": width3,
+                    "transition": "2s ease-in"
+                });
+            }, {
+
+                offset: width3
+            });
+
+            var width4 = $(".prog4").data("progress");
+            var progBar4 = $(".prog4");
+            skill.waypoint(function() {
+                progBar4.css({
+                    "width": width4,
+                    "transition": "2s ease-in"
+                });
+            }, {
+
+                offset: width4
+            });
+
+
+
+            /*
+             * -----------------------------------------------------------------
+             *-------------------------------Mixit Up---------------------------
+             * -----------------------------------------------------------------
+             */
+
+
+
+            var portfolioContent = $('.portfolio-content');
+
+            portfolioContent.mixItUp();
+
+
+
+            /*
+             * -----------------------------------------------------------------
+             *-----------------------------lightbox-----------------------------
+             * -----------------------------------------------------------------
+             */
+
+
+
+            lightbox.option({
+                'resizeDuration': 200,
+                'wrapAround': true,
+                showImageNumberLabel: false,
+            });
+
+
+            /*
+             * -----------------------------------------------------------------
+             *-----------------------------Ajax Chimp---------------------------
+             * -----------------------------------------------------------------
+             */
+
+
+            var chimpForm = $('#mc-form');
+
+            chimpForm.ajaxChimp({
+                url: 'https://tahsinscreation.us17.list-manage.com/subscribe/post?u=8d43c37bb4dbc0d0b8078d874&amp;id=c638bf1e04'
+            });
+
+
+
+
+            /*
+             * -----------------------------------------------------------------
+             *----------------------Contact form ajax---------------------------
+             * -----------------------------------------------------------------
+             */
+
+
+
+            var contactSubmit = $('#contact-submit');
+
+            contactSubmit.on('click', function(e) {
+                e.preventDefault();
+                var name = $('#form-name').val();
+                var email = $('#form-email').val();
+                var subject = $('#form-subject').val();
+                var message = $('#form-message').val();
+                var form = new Array({
+                    'name': name,
+                    'email': email,
+                    'subject': subject,
+                    'message': message
+                });
+                $.ajax({
+                    type: 'POST',
+                    url: "contact.php",
+                    data: ({
+                        'action': 'contact',
+                        'form': form
+                    })
+                }).done(function(data) {
+
+                    var conResult = $('#contact .result');
+                    conResult.html(data);
+                    $(".contact-form-area")[0].reset();
+
+                });
+
+            });
+
+
+            /*
+             * -----------------------------------------------------------------
+             *--------------------Owl Carousel For Testimonial------------------
+             * -----------------------------------------------------------------
+             */
+
+
+
+            var testimonial = $("#client-testimonial #owl-demo-testimonial");
+
+            testimonial.owlCarousel({
+                autoplay: true,
+                autoplayTimeout: 5000,
+                items: 2,
+                autoPlay: 6000,
+                responsive: {
+                    // breakpoint from 0 up
+                    0: {
+                        items: 1
+                    },
+                    // breakpoint from 480 up
+                    600: {
+                        items: 1
+                    },
+                    // breakpoint from 768 up
+                    900: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
+                },
+                pauseOnHover: true,
+                nav: true,
+                dots: false,
+                navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"]
+            });
+
+            var blog = $("#blog #owl-demo-blog");
+
+            blog.owlCarousel({
+                autoplay: true,
+                autoplayTimeout: 5000,
+                items: 1,
+                autoPlay: 6000,
+                responsive: {
+                    // breakpoint from 0 up
+                    0: {
+                        items: 1
+                    },
+                    // breakpoint from 480 up
+                    600: {
+                        items: 1
+                    },
+                    // breakpoint from 768 up
+                    900: {
+                        items: 1
+                    },
+                    1000: {
+                        items: 1
+                    }
+                },
+                pauseOnHover: true,
+                nav: true,
+                dots: false,
+                navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"]
+            });
+
+
+
+
+            /*
+             * -----------------------------------------------------------------
+             *-----------------------------Typed Js-----------------------------
+             * -----------------------------------------------------------------
+             */
+
+            if($(".welcome-area").is(".animated-text")) {
+                var typed = new Typed("#typed", {
+                    stringsElement: '#typed-strings',
+                    typeSpeed: 60,
+                    backSpeed: 30,
+                    backDelay: 2000,
+                    startDelay: 1000,
+                    loop: true
+
+                });
+            }
+
         }
-      }
+        /*
+         * -----------------------------------------------------------------
+         *----------------------- Pagepiling.js------------------------------
+         * -----------------------------------------------------------------
+         */
+
+
+        var pagepiling = $('#pagepiling');
+        pagepiling.pagepiling({
+            menu: '#mainMenu',
+            direction: 'vertical',
+            verticalCentered: true,
+            sectionsColor: [],
+            anchors: ['home', 'about', 'service', 'resume', 'portfolio', 'work', 'client-testimonial', 'blog', 'contact'],
+            scrollingSpeed: 700,
+            easing: 'swing',
+            loopBottom: true,
+            loopTop: true,
+            css3: true,
+            navigation: {
+                'textColor': '#000',
+                'bulletsColor': '#000',
+                'position': 'right'
+            },
+            normalScrollElements: null,
+            normalScrollElementTouchThreshold: 5,
+            touchSensitivity: 5,
+            keyboardScrolling: true,
+            sectionSelector: '.section',
+            animateAnchor: true,
+
+            //events
+            afterRender: function() {},
+            afterLoad: function(anchorLink, index) {}
+        });
+
+        /*
+         * -----------------------------------------------------------------
+         *----------------------- header info bar---------------------------
+         * -----------------------------------------------------------------
+         */
+
+
+        var headerInfo = $("#header-info-btn");
+
+        headerInfo.animatedModal({
+            modalTarget: 'header-info-bar',
+            animatedIn: 'fadeInRight',
+            animatedOut: 'fadeOutRight',
+        });
+
+        /*
+         * -----------------------------------------------------------------
+         *--------------------MagnificPopup---------------------------------
+         * -----------------------------------------------------------------
+         */
+
+
+
+        var videoPlay = $('.video-play');
+        videoPlay.magnificPopup({
+            type: 'iframe',
+            closeBtnInside: false,
+            closeOnContentClick: true,
+            tLoading: '', // remove text from preloader
+        });
+
+        /*
+         * -----------------------------------------------------------------
+         *-------------------------Vanta.js---------------------------------
+         * -----------------------------------------------------------------
+         */
+
+        if($(".vanta-bg").is(".bird")) {
+            VANTA.BIRDS({
+                el: "#welcome-vanta-area",
+                backgroundColor: 0xc0baa6,
+                color1: 0x0,
+                color2: 0x0,
+                colorMode: "lerp",
+                wingSpan: 14.00,
+                separation: 59.00,
+                cohesion: 17.00,
+                quantity: 3.00,
+                backgroundAlpha: 0.00
+            })
+
+        }
+        if($(".vanta-bg").is(".fog")) {
+            VANTA.FOG({
+                el: "#welcome-vanta-area",
+                highlightColor: 0x9ec8e6,
+                midtoneColor: 0x979acf,
+                baseColor: 0x141414,
+                blurFactor: 0.61,
+                speed: 2.60
+            })
+        }
+
+        if($(".vanta-bg").is(".waves")) {
+            VANTA.WAVES({
+                el: "#welcome-vanta-area",
+                color: 0x19303e,
+                zoom: 1.22
+            })
+        }
+
+        if($(".vanta-bg").is(".cloud")) {
+            VANTA.CLOUDS({
+                el: "#welcome-vanta-area",
+                skyColor: 0x1a1d34,
+                cloudColor: 0x5e697a,
+                cloudShadowColor: 0xb1020
+            })
+        }
+
     });
-  };
 
-
-  // Call the functions
-  magnifPopup();
-
-});
-
-// ========================================================================= //
-//  Porfolio isotope and filter
-// ========================================================================= //
-$(window).load(function () {
-
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
-
-  $('#portfolio-flters li').on('click', function () {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-
-    portfolioIsotope.isotope({
-      filter: $(this).data('filter')
-    });
-  });
-
-})
+})(jQuery);
